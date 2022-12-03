27aae9520388015dcbf44ec5e29cb942725f8e45
skylidge <47607002+skylidge@users.noreply.github.com>
2022-12-03 10:24:15 +0000

    completed day 3

---
+++ day3_part1.py
@@ -0,0 +1,24 @@
+import numpy as np
+from collections import Counter
+
+with open('data.csv') as f:
+    rucksacks = []
+    for line in f:
+        rucksack = line.strip()
+        compartment_1 = rucksack[:len(rucksack)//2]
+        compartment_2 = rucksack[len(rucksack)//2:]
+        rucksacks.append((compartment_1, compartment_2))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+priorities = np.arange(1,53)
+
+priority_map = {letter : priority for letter, priority in zip(alphabet, priorities)}
+
+score = 0
+for rucksack in rucksacks:
+    compartment_1 = Counter(rucksack[0])
+    compartment_2 = Counter(rucksack[1])
+    common_letter = np.unique(list((compartment_1 & compartment_2).elements()))[0]
+    score += priority_map[common_letter]
+
+print(score)
---
+++ day3_part2.py
@@ -0,0 +1,31 @@
+import numpy as np
+from collections import Counter
+
+
+with open('data.csv') as f:
+    groups = []
+    i = 1
+    group = []
+    for line in f:
+        if i%4 == 0:
+            groups.append(group)
+            i = 1
+            group = []
+        group.append(line.strip())
+        i += 1
+groups.append(group)
+        
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+priorities = np.arange(1,53)
+priority_map = {letter : priority for letter, priority in zip(alphabet, priorities)}
+
+score = 0
+for group in groups:
+    elf_1 = Counter(group[0])
+    elf_2 = Counter(group[1])
+    elf_3 = Counter(group[2])
+    
+    common_letter = np.unique(list((elf_1 & elf_2 & elf_3).elements()))[0]
+    score += priority_map[common_letter]
+
+print(score)
