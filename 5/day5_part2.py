416b67632692434744cdb8084857934e3e42af68
skylidge <47607002+skylidge@users.noreply.github.com>
2022-12-05 13:14:26 +0000

    completed day 5

---
+++ 5/day5_part2.py
@@ -0,0 +1,32 @@
+
+#stacks =     [['Z','N'],['M','C','D'],['P']]
+
+stacks = [['N','D','M','Q','B','P','Z'],
+          ['C','L','Z','Q','M','D','H','V'],
+          ['Q','H','R','D','V','F','Z','G'],
+          ['H','G','D','F','N'],
+          ['N','F','Q'],
+          ['D','Q','V','Z','F','B','T'],
+          ['Q','M','T','Z','D','V','S','H'],
+          ['M','G','F','P','N','Q'],
+          ['B','W','R','M']
+        ]
+
+with open('5/data.csv') as f:
+    instructions = []
+    for line in f:
+        txt = line.strip()
+        instruction = [int(s) for s in txt.split() if s.isdigit()]
+        instructions.append(instruction)
+
+for instruction in instructions:
+    stacks[instruction[2]-1] = stacks[instruction[2]-1] + stacks[instruction[1]-1][-instruction[0]:]
+    del stacks[instruction[1]-1][-instruction[0]:]
+
+
+print(stacks)
+final_letters = ""
+for stack in stacks:
+    final_letters += stack[-1]
+
+print(final_letters)
