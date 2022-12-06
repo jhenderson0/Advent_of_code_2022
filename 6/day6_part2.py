076a152f7111df1374a7fa5862f151b8e4746ff9
skylidge <47607002+skylidge@users.noreply.github.com>
2022-12-06 10:26:12 +0000

    completed day 6

---
+++ 6/day6_part2.py
@@ -0,0 +1,22 @@
+with open('6/data.csv') as f:
+    signals = []
+    for line in f:
+        signals.append(line.strip())
+
+score = []
+for signal in signals:
+    buffer = []
+    seen_chars = 0
+    for char in signal:
+        seen_chars += 1
+        buffer.append(char)
+        
+        if len(buffer) == 14:
+
+            if len(buffer) == len(set(buffer)):
+                score.append(seen_chars)
+                break
+            
+            buffer.pop(0)
+
+print(score)
